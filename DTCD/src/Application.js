0b5fc9437823b95bef96fde7e4a5028b0f9bf323
romankuramshin <rkuramshin@isgneuro.com>
2021-05-11 16:17:59 +0300

    removed console.logs

--- DTCD/src/Application.js
+++ DTCD/src/Application.js
@@ -168,11 +168,8 @@ export default class Application {
 
   uninstallPluginByInstance(instance) {
     const guid = Object.keys(this.#guids).find(key => this.#guids[key] === instance);
-    console.log(guid);
     // for autocomplete
-    console.log(Object.keys(this.#autocomplete));
     const key = Object.keys(this.#autocomplete).find(instanceName => instanceName.endsWith(`_${guid}`));
-    console.log(key);
 
     delete this.#autocomplete[key];
     delete this.#guids[guid];
