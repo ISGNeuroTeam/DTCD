6e40a56e9ac668105f1c3f78505825be2f82a9c5
ilya <isagaidak@isgneuro.com>
2021-11-03 16:48:18 +0300

    mockserver minor fix

--- server/main.py
+++ server/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi.responses import HTMLResponse
 
-import uvicorn 
+import uvicorn
 
 app = FastAPI()
 
@@ -20,9 +20,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["DELETE","GET","POST","PUT"],
+    allow_methods=["DELETE", "GET", "POST", "PUT"],
     allow_headers=["*"]
 )
+
 
 # Workspaces
 def get_workspace_list():
@@ -31,18 +32,20 @@ def get_workspace_list():
     if ".DS_Store" in list_dir_workspaces:
         list_dir_workspaces.remove(".DS_Store")
 
-    workspaces=[]
+    workspaces = []
     for file_name in list_dir_workspaces:
         with open(f'./workspaces/{file_name}', "r") as file:
-            configuration = json.loads(file.read())
+            configuration = json.load(file)
             workspaces.append(configuration)
     return workspaces
+
 
 # Application
 @app.get("/", response_class=HTMLResponse)
 async def read_html():
-    with open("./../public/index.html",'r') as file:
+    with open("./../public/index.html", 'r') as file:
         return file.read()
+
 
 @app.get("/plugins/plugins.json")
 def read_plugins_list():
@@ -50,37 +53,44 @@ def read_plugins_list():
     list_dir.remove(".gitkeep")
     if ".DS_Store" in list_dir:
         list_dir.remove(".DS_Store")
+
     def item_to_path(item):
-        if(os.path.isdir("./plugins/%s"%item)):
-            js_files = [file_name for file_name in os.listdir("./plugins/%s" % item) if file_name.endswith(".js") ]
-            return "./{0}/{1}".format(item, js_files[0])
+        if os.path.isdir("./plugins/%s" % item):
+            js_file = next(file_name for file_name in os.listdir("./plugins/%s" % item) if file_name.endswith(".js"))
+            return "./{0}/{1}".format(item, js_file)
+
     return [item_to_path(item) for item in list_dir]
+
 
 @app.get("/get-dependence-list")
 def read_dependencies_manifest():
-    with open("./dependencies/manifest.json","r") as file:
+    with open("./dependencies/manifest.json", "r") as file:
         data = json.load(file)
         return data
 
-#logs
+
+# logs
 @app.get("/v2/logs/configuration")
 def log_config():
-    with open("./log_configuration.json","r") as file:
+    with open("./log_configuration.json", "r") as file:
         data = json.load(file)
         return data
+
 
 @app.post("/v2/logs/save")
 def logs_save(logs: list = Body(...)):
-    with open("./calc_log.txt",'w') as file:
+    with open("./calc_log.txt", 'w') as file:
         file.write(str(logs))
     return 'saved!'
 
-#StyleSystem
+
+# StyleSystem
 @app.get("/get-design-objects")
 def design_obejct():
-    with open("./Design_objects.json","r") as file:
+    with open("./Design_objects.json", "r") as file:
         data = json.load(file)
         return data
+
 
 # graph
 @app.post("/v2/graphContent/save")
@@ -88,13 +98,13 @@ def save_graph(graphs: list = Body(...)):
     count = 0
     for graph in graphs:
         if not (graph["name"]) in os.listdir("./graphs"):
-            with open(os.path.join("./graphs/",graph["name"]), "w") as file:
+            with open(os.path.join("./graphs/", graph["name"]), "w") as file:
                 file.write(graph["content"])
-            count+=1
-    if count==len(graphs):
-        return {"status":"SUCCESS", "code":200, "count": str(len(graphs))}
-    else:
-        return {"status":"ERROR", "count": count}
+            count += 1
+    if count == len(graphs):
+        return {"status": "SUCCESS", "code": 200, "count": count}
+    return {"status": "ERROR", "count": count}
+
 
 @app.put("/v2/graphContent/update")
 def update_graph(graphs: list = Body(...)):
@@ -104,31 +114,30 @@ def update_graph(graphs: list = Body(...)):
     for graph in graphs:
         if file_list[graph["id"]]:
             if "content" in graph:
-                with open(os.path.join("./graphs/",graph["name"]), "w") as file:
+                with open(os.path.join("./graphs/", file_list[graph["id"]]), "w") as file:
                     file.write(graph["content"])
-            if "name" in graph:
+            if "name" in graph and graph["name"] != file_list[graph["id"]]:
                 source = "./graphs/%s" % file_list[graph["id"]]
                 target = "./graphs/%s" % graph["name"]
                 os.rename(source, target)
-            count+=1
-    if count==len(graphs):
-        return {"status":"SUCCESS","count":count}
-    else:
-        return {"status":"ERROR", "count": count}
+            count += 1
+    if count == len(graphs):
+        return {"status": "SUCCESS", "count": count}
+    return {"status": "ERROR", "count": count}
+
 
 @app.delete("/v2/graphContent/delete")
 def delete_graph(ids: list = Body(...)):
-    print(ids)
     file_list = os.listdir("./graphs")
     file_list.remove(".gitkeep")
-    count=0
+    count = 0
     for id in ids:
         os.remove("./graphs/%s" % file_list[id])
-        count+=1
+        count += 1
     if count == len(ids):
-        return {"status":"SUCCESS","count": count}
-    else:
-        return {"status":"ERROR", "count": count}
+        return {"status": "SUCCESS", "count": count}
+    return {"status": "ERROR", "count": count}
+
 
 @app.get("/v2/graphContent/load")
 def load_graph(id: int = 0):
@@ -138,20 +147,23 @@ def load_graph(id: int = 0):
         content = file.read()
     return {"id": id, "name": file_list[id], "content": content, "status": "SUCCESS"}
 
+
 @app.get("/v2/graph/list")
 def graph_list():
     file_list = os.listdir("./graphs")
     file_list.remove(".gitkeep")
-    return [ {"name": file_name, "id": index} for index, file_name in enumerate(file_list)]
+    return [{"name": file_name, "id": index} for index, file_name in enumerate(file_list)]
+
 
 @app.get("/v2/workspace/object")
 def workspace_configuration(id: int = 0):
     workspaces = get_workspace_list()
     if not id:
-        mapped_confs = list(map(lambda conf: {'id':conf['id'], 'title':conf['title']}, workspaces))
+        mapped_confs = list(map(lambda conf: {'id': conf['id'], 'title': conf['title']}, workspaces))
         return mapped_confs
     conf = next(conf for conf in workspaces if conf["id"] == id)
     return conf
+
 
 @app.post("/v2/workspace/object")
 def create_workspace(workspaces: list = Body(...)):
@@ -167,38 +179,39 @@ def create_workspace(workspaces: list = Body(...)):
             file.write(json.dumps(configuration))
     return workspaces
 
+
 @app.put("/v2/workspace/object")
 def update_workspace(workspaces: list = Body(...)):
     file_list = os.listdir("./workspaces")
     edited = []
     for conf in workspaces:
         file_name = f"{conf['id']}.json"
-        if file_list.index(file_name) != -1:
-            if not conf['content']:
+        if file_name in file_list:
+            if 'content' not in conf:  # if no content change title
                 configuration = ''
                 with open(f'./workspaces/{file_name}', "r") as file:
-                    configuration = json.loads(file.read())
-                os.remove(os.path.join("./workspaces",file_name))
+                    configuration = json.load(file)
                 configuration['title'] = conf['title']
-                with open(os.path.join("./workspaces", f"{conf['id']}.json"), "w") as file:
+                with open(os.path.join("./workspaces", file_name), "w") as file:
                     file.write(json.dumps(configuration))
             else:
-                os.remove(os.path.join("./workspaces",file_name))
-                with open(os.path.join("./workspaces", f"{conf['id']}.json"), "w") as file:
+                with open(os.path.join("./workspaces", file_name), "w") as file:
                     file.write(json.dumps(conf))
             edited.append(conf['id'])
     return edited
+
 
 @app.delete("/v2/workspace/object")
 def delete_workspace(idxes: list = Body(...)):
     file_list = os.listdir("./workspaces")
     for idx in idxes:
         file_name = f"{idx}.json"
-        if file_list.index(file_name) != -1:
-            os.remove(os.path.join("./workspaces",file_name))
+        if file_name in file_list:
+            os.remove(os.path.join("./workspaces", file_name))
         else:
             return 'error'
     return 'success'
+
 
 # GIS
 @app.get("/get-gis-data")
